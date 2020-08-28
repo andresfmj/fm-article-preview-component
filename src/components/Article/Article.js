264e74246c9139ea72769d7a39fcc6e87bde3fe7
Andres Fernando <andres.fernando08@gmail.com>
2020-08-27 22:13:30 -0500

    created Article component and linked into the App component

---
+++ src/components/Article/Article.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import "./Article.scss";
+
+function Article(props) {
+
+    return (
+        <div className='Article'>
+            <div className="Article-inner">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Article;
